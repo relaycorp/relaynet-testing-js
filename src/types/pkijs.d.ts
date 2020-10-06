0a4859f30376a83f083eb2baed97e4a2c1597789
Gus Narea <gnarea@users.noreply.github.com>
2020-10-06 15:28:56 +0100

    chore(deps): Upgrade Relaynet (#5)

--- src/types/pkijs.d.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable:no-implicit-dependencies no-submodule-imports */
-
-// Workaround for https://github.com/relaycorp/relaynet-core-js/issues/52
-
-declare module 'pkijs' {
-  export { default as Certificate } from 'pkijs/src/Certificate';
-  export { default as EnvelopedData } from 'pkijs/src/EnvelopedData';
-}
