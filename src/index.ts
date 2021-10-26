0db81fe74f8c0e604f418a03d26122b5bec72984
Gus Narea <gnarea@users.noreply.github.com>
2021-10-26 14:15:08 +0100

    feat: Implement method to generate session key pairs for all nodes
    BREAKING CHANGE: `generateNodeKeyPairSet()` is now `generateIdentityKeyPairSet()`.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 export { generateCDACertificationPath, CDACertPath } from './lib/CDACertPath';
 export { generatePDACertificationPath, PDACertPath } from './lib/PDACertPath';
-export { generateNodeKeyPairSet, NodeKeyPairSet } from './lib/NodeKeyPairSet';
+export {
+  generateIdentityKeyPairSet,
+  generateSessionKeyPairSet,
+  NodeKeyPairSet,
+} from './lib/NodeKeyPairSet';
 
 export { MockGSCClient } from './lib/gsc/MockGSCClient';
 export * from './lib/gsc/methodCalls';
